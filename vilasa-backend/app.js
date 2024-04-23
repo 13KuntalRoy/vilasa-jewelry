52b915cf977a57eb9f05fe5475bfee27c3ea9ac6
13KuntalRoy <roykuntal1213@gmail.com>
2024-04-24 04:48:17 +0530

    Access-Control-Allow-Origin

--- vilasa-backend/app.js
+++ vilasa-backend/app.js
@@ -42,8 +42,15 @@ app.use(cors(
 
 
   }
-)); // Middleware for enabling CORS
+)); 
 
+// Middleware for enabling CORS
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+})
 // Custom error handling middleware
 app.use(errorMiddleware);
 
