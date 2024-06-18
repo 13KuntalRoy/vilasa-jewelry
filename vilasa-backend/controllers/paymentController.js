fd5c85e1eb86bbc1a92e5ec9ce5007911b0c19a7
13KuntalRoy <roykuntal1213@gmail.com>
2024-06-18 16:09:45 +0530

    "Fixed typo in variable name and added optional chaining to entity assignment"

--- vilasa-backend/controllers/paymentController.js
+++ vilasa-backend/controllers/paymentController.js
@@ -174,7 +174,7 @@ exports.razorpayWebhook = asyncWrapper(async (req, res, next) => {
   try {
     console.log(responce);
     const event = responce.event;
-    const entity =  responce.payload.payment.entity;
+    const entity =  response?.payload?.payment?.entity;
 
     // Handle the event based on event type
     switch (event) {
