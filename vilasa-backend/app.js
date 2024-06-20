713c3ee132ecc1377d18bbf15d2f8e51e9fa8cb2
13KuntalRoy <roykuntal1213@gmail.com>
2024-06-20 21:23:04 +0530

    "Refactored app.js and server.js files, reorganized code, and removed commented out lines."

--- vilasa-backend/app.js
+++ vilasa-backend/app.js
@@ -1,3 +1,133 @@
+const express = require("express");
+const http = require('http');
+const dotenv = require("dotenv");
+const connectDatabase = require("./database/connectdatabase");
+const cloudinary = require("cloudinary");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const listEndpoints = require('express-list-endpoints');
+const ErrorHandler = require('./utils/errorHandler');
+const axios = require('axios'); 
+dotenv.config({ path: "vilasa-backend/config/config.env" });
+
+const app = express();
+const httpServer = http.createServer(app);
+
+// Middleware setup
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json({ limit: "100mb", type: 'application/json' }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true, type: 'application/x-www-form-urlencoded' }));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+    limits: { fileSize: 100 * 1024 * 1024 },
+}));
+app.use(cors({
+    origin: ["http://localhost:5173", "https://vilasajewelry.netlify.app", "http://localhost:3000", "http://localhost"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+}));
+
+// Connect to MongoDB
+connectDatabase();
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+});
+
+// Route setup
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const product = require("./routes/productRoute");
+const payment = require("./routes/paymentRoute");
+const chat = require("./routes/chatRoute");
+const dyimg = require("./routes/dynamicImageRoutes");
+const dyurl = require("./routes/urlRoutes");
+const invoice = require("./routes/invoiceRoute");
+const enquiry = require("./routes/enquiryRoute");
+const subscriber = require('./routes/subscriberRouter');
+const contact = require('./routes/contactRoute');
+const teamProfile = require('./routes/teamprofileRoute');
+const cart = require('./routes/cartRoute');
+const wishlist = require('./routes/wishlistRoute');
+const cal = require('./routes/calRoute');
+const videos = require('./routes/videoRoute');
+
+app.use("/api/vilasa-v1/vproduct", product);
+app.use("/api/vilasa-v1/user", user);
+app.use("/api/vilasa-v1/order", order);
+app.use("/api/vilasa-v1/payment", payment);
+app.use("/api/vilasa-v1/chat/v.0.0.1", chat);
+app.use("/api/vilasa-v1/db-img/vilasa", dyimg);
+app.use("/api/vilasa-v1/db-url/vilasa", dyurl);
+app.use("/api/vilasa-v1/order/report", invoice);
+app.use("/api/vilasa-v1/venquiry/f1", enquiry);
+app.use("/api/vilasa-v1/subscriberapi", subscriber);
+app.use("/api/vilasa-v1/contact/v1", contact);
+app.use("/api/vilasa-v1/teamProfile/v1", teamProfile);
+app.use("/api/vilasa-v1/cart/v1", cart);
+app.use("/api/vilasa-v1/wishlist/v1", wishlist);
+app.use("/api/vilasa-v1/dashboard", cal);
+app.use("/api/vilasa-v1/video", videos);
+
+// Middleware to expose endpoint list only in development mode
+const environment = process.env.NODE_ENV || 'development';
+
+if (environment === 'development') {
+    app.get('/api/Secrete/endpoints', (req, res) => {
+        console.log('Middleware triggered. Getting endpoints...');
+        const endpoints = listEndpoints(app);
+        console.log('Endpoints retrieved:', endpoints);
+        res.json(endpoints);
+    });
+}
+
+app.get('/api/pincodeserviceable', async (req, res) => {
+    try {
+        const pincode = req.query.pincode;
+        const response = await axios.get(`https://app.shipway.com/api/pincodeserviceable?pincode=${pincode}&payment_type=P`, {
+            headers: {
+                'Authorization': req.headers.authship  // Forward the Authorization header
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error proxying request:', error);
+        res.status(500).json({ error: 'Failed to proxy request' });
+    }
+});
+
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+    if (err instanceof ErrorHandler) {
+        res.status(err.statusCode).json(err.toJSON());
+    } else {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: {
+                message: 'Internal Server Error',
+                statusCode: 500
+            }
+        });
+    }
+});
+
+// Simple middleware for logging each request to the console
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+module.exports = { app, httpServer };
+
+
 // const express = require("express");
 // const app = express();
 // const errorMiddleware = require("./middleware/error");
@@ -115,182 +245,182 @@
 // });
 
 // module.exports = app;
-const express = require("express");
-const http = require('http');
-const { Server } = require('socket.io');
-const dotenv = require("dotenv");
-const connectDatabase = require("./database/connectdatabase");
-const cloudinary = require("cloudinary");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
-const path = require("path");
-const cors = require("cors");
-const cronSchedule = require('./Schedule');
-const listEndpoints = require('express-list-endpoints');
-const ErrorHandler = require('./utils/errorHandler');
-const axios = require('axios'); 
-dotenv.config({ path: "vilasa-backend/config/config.env" });
+// const express = require("express");
+// const http = require('http');
+// const { Server } = require('socket.io');
+// const dotenv = require("dotenv");
+// const connectDatabase = require("./database/connectdatabase");
+// const cloudinary = require("cloudinary");
+// const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// const fileUpload = require("express-fileupload");
+// const path = require("path");
+// const cors = require("cors");
+// const cronSchedule = require('./Schedule');
+// const listEndpoints = require('express-list-endpoints');
+// const ErrorHandler = require('./utils/errorHandler');
+// const axios = require('axios'); 
+// dotenv.config({ path: "vilasa-backend/config/config.env" });
 
-const app = express();
-const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-    cors: {
-        origin: [
-            "http://localhost:5173",
-            "https://vilasajewelry.netlify.app",
-            "http://localhost:3000",
-            "http://localhost"
-        ],
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        credentials: true,
-    }
-});
+// const app = express();
+// const httpServer = http.createServer(app);
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: [
+//             "http://localhost:5173",
+//             "https://vilasajewelry.netlify.app",
+//             "http://localhost:3000",
+//             "http://localhost"
+//         ],
+//         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//         credentials: true,
+//     }
+// });
 
-// Middleware setup
-app.use(cookieParser());
-app.use(express.json());
-app.use(bodyParser.json({ limit: "100mb", type: 'application/json' }));
-app.use(bodyParser.urlencoded({ limit: "100mb", extended: true, type: 'application/x-www-form-urlencoded' }));
-app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: '/tmp/',
-    limits: { fileSize: 100 * 1024 * 1024 },
-}));
-app.use(cors({
-    origin:["http://localhost:5173","https://vilasajewelry.netlify.app","http://localhost:3000","http://localhost"],
-    methods:["GET","POST","PUT","DELETE","PATCH"],
-    credentials:true
-}));
+// // Middleware setup
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(bodyParser.json({ limit: "100mb", type: 'application/json' }));
+// app.use(bodyParser.urlencoded({ limit: "100mb", extended: true, type: 'application/x-www-form-urlencoded' }));
+// app.use(fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: '/tmp/',
+//     limits: { fileSize: 100 * 1024 * 1024 },
+// }));
+// app.use(cors({
+//     origin:["http://localhost:5173","https://vilasajewelry.netlify.app","http://localhost:3000","http://localhost"],
+//     methods:["GET","POST","PUT","DELETE","PATCH"],
+//     credentials:true
+// }));
 
 
-// Connect to MongoDB
-connectDatabase();
+// // Connect to MongoDB
+// connectDatabase();
 
-// Configure Cloudinary
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-});
+// // Configure Cloudinary
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_key: process.env.API_KEY,
+//     api_secret: process.env.API_SECRET,
+// });
 
-// Socket.IO connection handler
-io.on('connection', (socket) => {
-    console.log('A user connected', socket.id);
+// // Socket.IO connection handler
+// io.on('connection', (socket) => {
+//     console.log('A user connected', socket.id);
 
-    // Handle chat message event
-    socket.on('chat_message', ({ room, msg }) => {
-        console.log('Message: ' + msg);
-        socket.to(room).emit("receive-message", msg);
-    });
+//     // Handle chat message event
+//     socket.on('chat_message', ({ room, msg }) => {
+//         console.log('Message: ' + msg);
+//         socket.to(room).emit("receive-message", msg);
+//     });
 
-    socket.on('enquiry', (enquiry) => {
-        console.log('New enquiry:', enquiry);
-        io.emit("New_Enquiry", enquiry);
-    });
+//     socket.on('enquiry', (enquiry) => {
+//         console.log('New enquiry:', enquiry);
+//         io.emit("New_Enquiry", enquiry);
+//     });
 
-    socket.on("join-room", (room) => {
-        socket.join(room);
-        console.log(`User joined room ${room}`);
-    });
+//     socket.on("join-room", (room) => {
+//         socket.join(room);
+//         console.log(`User joined room ${room}`);
+//     });
 
-    // Handle status change
-    socket.on('status_change', (updatedEnquiry) => {
-        console.log('Status change:', updatedEnquiry);
-        io.emit("Enquiry_Status_Changed", updatedEnquiry);
-    });
+//     // Handle status change
+//     socket.on('status_change', (updatedEnquiry) => {
+//         console.log('Status change:', updatedEnquiry);
+//         io.emit("Enquiry_Status_Changed", updatedEnquiry);
+//     });
 
-    socket.on("deleteEnquiryById", (enquiryId) => {
-        console.log(`Enquiry deleted: ${enquiryId}`);
-        io.emit("delete-enquiry", enquiryId);
-    });
+//     socket.on("deleteEnquiryById", (enquiryId) => {
+//         console.log(`Enquiry deleted: ${enquiryId}`);
+//         io.emit("delete-enquiry", enquiryId);
+//     });
 
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
+//     // Handle disconnection
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected');
+//     });
+// });
 
-// Route setup
-const user = require("./routes/userRoute");
-const order = require("./routes/orderRoute");
-const product = require("./routes/productRoute");
-const payment = require("./routes/paymentRoute");
-const chat = require("./routes/chatRoute");
-const dyimg = require("./routes/dynamicImageRoutes");
-const dyurl = require("./routes/urlRoutes");
-const invoice = require("./routes/invoiceRoute");
-const enquiry = require("./routes/enquiryRoute")
-const subscriber = require('./routes/subscriberRouter');
-const contact = require('./routes/contactRoute')
-const teamProfile = require('./routes/teamprofileRoute')
-const cart = require('./routes/cartRoute')
-const wishlist = require('./routes/wishlistRoute')
-const cal = require('./routes/calRoute')
-const videos = require('./routes/videoRoute')
+// // Route setup
+// const user = require("./routes/userRoute");
+// const order = require("./routes/orderRoute");
+// const product = require("./routes/productRoute");
+// const payment = require("./routes/paymentRoute");
+// const chat = require("./routes/chatRoute");
+// const dyimg = require("./routes/dynamicImageRoutes");
+// const dyurl = require("./routes/urlRoutes");
+// const invoice = require("./routes/invoiceRoute");
+// const enquiry = require("./routes/enquiryRoute")
+// const subscriber = require('./routes/subscriberRouter');
+// const contact = require('./routes/contactRoute')
+// const teamProfile = require('./routes/teamprofileRoute')
+// const cart = require('./routes/cartRoute')
+// const wishlist = require('./routes/wishlistRoute')
+// const cal = require('./routes/calRoute')
+// const videos = require('./routes/videoRoute')
 
-app.use("/api/vilasa-v1/vproduct", product);
-app.use("/api/vilasa-v1/user", user);
-app.use("/api/vilasa-v1/order", order);
-app.use("/api/vilasa-v1/payment", payment);
-app.use("/api/vilasa-v1/chat/v.0.0.1",chat);
-app.use("/api/vilasa-v1/db-img/vilasa", dyimg);
-app.use("/api/vilasa-v1/db-url/vilasa", dyurl);
-app.use("/api/vilasa-v1/order/report",invoice);
-app.use("/api/vilasa-v1/venquiry/f1",enquiry);
-app.use("/api/vilasa-v1/subscriberapi",subscriber);
-app.use("/api/vilasa-v1/contact/v1",contact);
-app.use("/api/vilasa-v1/teamProfile/v1",teamProfile);
-app.use("/api/vilasa-v1/cart/v1",cart);
-app.use("/api/vilasa-v1/wishlist/v1",wishlist);
-app.use("/api/vilasa-v1/dashboard",cal)
-app.use("/api/vilasa-v1/video", videos);
+// app.use("/api/vilasa-v1/vproduct", product);
+// app.use("/api/vilasa-v1/user", user);
+// app.use("/api/vilasa-v1/order", order);
+// app.use("/api/vilasa-v1/payment", payment);
+// app.use("/api/vilasa-v1/chat/v.0.0.1",chat);
+// app.use("/api/vilasa-v1/db-img/vilasa", dyimg);
+// app.use("/api/vilasa-v1/db-url/vilasa", dyurl);
+// app.use("/api/vilasa-v1/order/report",invoice);
+// app.use("/api/vilasa-v1/venquiry/f1",enquiry);
+// app.use("/api/vilasa-v1/subscriberapi",subscriber);
+// app.use("/api/vilasa-v1/contact/v1",contact);
+// app.use("/api/vilasa-v1/teamProfile/v1",teamProfile);
+// app.use("/api/vilasa-v1/cart/v1",cart);
+// app.use("/api/vilasa-v1/wishlist/v1",wishlist);
+// app.use("/api/vilasa-v1/dashboard",cal)
+// app.use("/api/vilasa-v1/video", videos);
 
-// Middleware to expose endpoint list only in development mode
-const environment = process.env.NODE_ENV || 'development';
+// // Middleware to expose endpoint list only in development mode
+// const environment = process.env.NODE_ENV || 'development';
 
-if (environment === 'development') {
-    app.get('/api/Secrete/endpoints', (req, res) => {
-        console.log('Middleware triggered. Getting endpoints...');
-        const endpoints = listEndpoints(app);
-        console.log('Endpoints retrieved:', endpoints);
-        res.json(endpoints);
-    });
-}
-app.get('/api/pincodeserviceable', async (req, res) => {
-  try {
-    const pincode = req.query.pincode;
-    const response = await axios.get(`https://app.shipway.com/api/pincodeserviceable?pincode=${pincode}&payment_type=P`, {
-      headers: {
-        'Authorization': req.headers.authship  // Forward the Authorization header
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error proxying request:', error);
-    res.status(500).json({ error: 'Failed to proxy request' });
-  }
-});
-// Middleware to handle errors
-app.use((err, req, res, next) => {
-    if (err instanceof ErrorHandler) {
-        res.status(err.statusCode).json(err.toJSON());
-    } else {
-        console.error(err);
-        res.status(500).json({
-            success: false,
-            error: {
-                message: 'Internal Server Error',
-                statusCode: 500
-            }
-        });
-    }
-});
+// if (environment === 'development') {
+//     app.get('/api/Secrete/endpoints', (req, res) => {
+//         console.log('Middleware triggered. Getting endpoints...');
+//         const endpoints = listEndpoints(app);
+//         console.log('Endpoints retrieved:', endpoints);
+//         res.json(endpoints);
+//     });
+// }
+// app.get('/api/pincodeserviceable', async (req, res) => {
+//   try {
+//     const pincode = req.query.pincode;
+//     const response = await axios.get(`https://app.shipway.com/api/pincodeserviceable?pincode=${pincode}&payment_type=P`, {
+//       headers: {
+//         'Authorization': req.headers.authship  // Forward the Authorization header
+//       }
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error('Error proxying request:', error);
+//     res.status(500).json({ error: 'Failed to proxy request' });
+//   }
+// });
+// // Middleware to handle errors
+// app.use((err, req, res, next) => {
+//     if (err instanceof ErrorHandler) {
+//         res.status(err.statusCode).json(err.toJSON());
+//     } else {
+//         console.error(err);
+//         res.status(500).json({
+//             success: false,
+//             error: {
+//                 message: 'Internal Server Error',
+//                 statusCode: 500
+//             }
+//         });
+//     }
+// });
 
-// Simple middleware for logging each request to the console
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
+// // Simple middleware for logging each request to the console
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();
+// });
 
-module.exports = { app, httpServer };
+// module.exports = { app, httpServer };
--- vilasa-backend/server.js
+++ vilasa-backend/server.js
@@ -1,3 +1,45 @@
+const { app, httpServer } = require('./app');
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config({ path: "vilasa-backend/config/config.env" });
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+    console.error(`Uncaught Exception: ${err.message}`);
+    console.error("Shutting down the server due to an uncaught exception");
+    process.exit(1);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+    console.error(`Unhandled Promise Rejection: ${err.message}`);
+    console.error("Shutting down the server due to an unhandled promise rejection");
+    httpServer.close(() => {
+        process.exit(1);
+    });
+});
+
+// Graceful shutdown on SIGINT signal (Ctrl+C)
+process.on('SIGINT', () => {
+    console.log('SIGINT received. Shutting down gracefully...');
+    httpServer.close(() => {
+        console.log('Server stopped.');
+        process.exit(0);
+    });
+});
+
+// Log when the server is closed unexpectedly
+httpServer.on('close', () => {
+    console.log('Server closed unexpectedly.');
+});
+
 // const app = require('./app');
 // const dotenv = require("dotenv");
 // const connectDatabase = require("./database/connectdatabase");
@@ -108,44 +150,44 @@
 // server.on('close', () => {
 //     console.log('Server closed unexpectedly.');
 // });
-const { app, httpServer } = require('./app');
-const dotenv = require("dotenv");
+// const { app, httpServer } = require('./app');
+// const dotenv = require("dotenv");
 
-// Load environment variables
-dotenv.config({ path: "vilasa-backend/config/config.env" });
+// // Load environment variables
+// dotenv.config({ path: "vilasa-backend/config/config.env" });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-httpServer.listen(PORT, () => {
-    console.log(`Server is listening on PORT ${PORT}`);
-});
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// httpServer.listen(PORT, () => {
+//     console.log(`Server is listening on PORT ${PORT}`);
+// });
 
-// Handle uncaught exceptions
-process.on("uncaughtException", (err) => {
-    console.error(`Uncaught Exception: ${err.message}`);
-    console.error("Shutting down the server due to an uncaught exception");
-    process.exit(1);
-});
+// // Handle uncaught exceptions
+// process.on("uncaughtException", (err) => {
+//     console.error(`Uncaught Exception: ${err.message}`);
+//     console.error("Shutting down the server due to an uncaught exception");
+//     process.exit(1);
+// });
 
-// Handle unhandled promise rejections
-process.on("unhandledRejection", (err) => {
-    console.error(`Unhandled Promise Rejection: ${err.message}`);
-    console.error("Shutting down the server due to an unhandled promise rejection");
-    httpServer.close(() => {
-        process.exit(1);
-    });
-});
+// // Handle unhandled promise rejections
+// process.on("unhandledRejection", (err) => {
+//     console.error(`Unhandled Promise Rejection: ${err.message}`);
+//     console.error("Shutting down the server due to an unhandled promise rejection");
+//     httpServer.close(() => {
+//         process.exit(1);
+//     });
+// });
 
-// Graceful shutdown on SIGINT signal (Ctrl+C)
-process.on('SIGINT', () => {
-    console.log('SIGINT received. Shutting down gracefully...');
-    httpServer.close(() => {
-        console.log('Server stopped.');
-        process.exit(0);
-    });
-});
+// // Graceful shutdown on SIGINT signal (Ctrl+C)
+// process.on('SIGINT', () => {
+//     console.log('SIGINT received. Shutting down gracefully...');
+//     httpServer.close(() => {
+//         console.log('Server stopped.');
+//         process.exit(0);
+//     });
+// });
 
-// Log when the server is closed unexpectedly
-httpServer.on('close', () => {
-    console.log('Server closed unexpectedly.');
-});
+// // Log when the server is closed unexpectedly
+// httpServer.on('close', () => {
+//     console.log('Server closed unexpectedly.');
+// });
